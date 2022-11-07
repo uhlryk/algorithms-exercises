0d0a73e54f83019e205b16494e717cece2ef264f
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2022-11-07 21:21:51 +0100

    calculate gcd iteration

---
+++ greatest-common-division/greatest-common-divisor.test.ts
@@ -0,0 +1,48 @@
+import 'jest';
+import { gcdRecursion, gcdIteration } from './greatest-common-divisor';
+
+describe('greatest-common-divisor', () => {
+    describe('recursion', () => {
+        it('should return gcd 13 for 39 26', async () => {
+            expect(gcdRecursion(39, 26)).toBe(13);
+        })
+
+        it('should return gcd 5 for 25 15', async () => {
+            expect(gcdRecursion(25, 15)).toBe(5);
+        })
+
+        it('should return gcd 14 for 42 56', async () => {
+            expect(gcdRecursion(42, 56)).toBe(14);
+        })
+
+        it('should throw error if args not integer', async () => {
+            expect(() => gcdRecursion(1.1, 2.4)).toThrow('Not a integer');
+        })
+
+        it('should throw error if args not integer', async () => {
+            expect(() => gcdRecursion(-11, 0)).toThrow('Not greater than 0');
+        })
+    })
+
+    describe('iteration', () => {
+        it('should return gcd 13 for 39 26', async () => {
+            expect(gcdIteration(39, 26)).toBe(13);
+        })
+
+        it('should return gcd 5 for 25 15', async () => {
+            expect(gcdIteration(25, 15)).toBe(5);
+        })
+
+        it('should return gcd 14 for 42 56', async () => {
+            expect(gcdIteration(42, 56)).toBe(14);
+        })
+
+        it('should throw error if args not integer', async () => {
+            expect(() => gcdIteration(1.1, 2.4)).toThrow('Not a integer');
+        })
+
+        it('should throw error if args not integer', async () => {
+            expect(() => gcdIteration(-11, 0)).toThrow('Not greater than 0');
+        })
+    })
+})
--- greatest-common-division/greatest-common-divisor.ts
+++ greatest-common-division/greatest-common-divisor.ts
@@ -15,3 +15,19 @@ export function gcdRecursion(a: number, b: number): number {
     }
     return gcdRecursion(a, b);
 }
+
+export function gcdIteration(a: number, b: number): number {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        throw new Error ('Not a integer');
+    }
+    if(a < 1 || b < 1) {
+        throw new Error('Not greater than 0');
+    }
+
+    while(a > 0 && b > 0) {
+        [a, b] = a > b ? [a, b] : [b , a];
+        a = a % b;
+    }
+
+    return a || b;
+}
