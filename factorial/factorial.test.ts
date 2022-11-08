03fe24a2492e6a817d79ab75dd5cabfd3ab15d89
Sztompka Krzysztof <sztompka.krzysztof@gmail.com>
2022-11-08 21:32:31 +0100

    factorial iteration

--- factorial/factorial.test.ts
+++ factorial/factorial.test.ts
@@ -1,6 +1,6 @@
 import 'jest';
 
-import { factorialRecursion } from './factorial';
+import { factorialRecursion, factorialIteration } from './factorial';
 
 describe('factorial', () => {
     describe('recursion', () => {
@@ -18,6 +18,23 @@ describe('factorial', () => {
         })
         it('should throw error for non integer', () => {
             expect(() => factorialRecursion(0.1)).toThrow('Not an integer');
+        })
+    })
+    describe('iteration', () => {
+        it('should return 6 for 3!', () => {
+            expect(factorialIteration(3)).toBe(6);
+        });
+        it('should return 24 for 4!', () => {
+            expect(factorialIteration(4)).toBe(24);
+        })
+        it('should return 120 for 5!', () => {
+            expect(factorialIteration(5)).toBe(120);
+        })
+        it('should return 1 for 1!', () => {
+            expect(factorialIteration(1)).toBe(1);
+        })
+        it('should throw error for non integer', () => {
+            expect(() => factorialIteration(0.1)).toThrow('Not an integer');
         })
     })
 })
--- factorial/factorial.ts
+++ factorial/factorial.ts
@@ -8,3 +8,17 @@ export function factorialRecursion(num: number): number {
 
     return num * factorialRecursion(num - 1);
 }
+
+export function factorialIteration(num: number) :number {
+    if(!Number.isInteger(num)) {
+        throw new Error('Not an integer')
+    }
+    if(num === 1 || num === 0) {
+        return 1;
+    }
+    let res = 1;
+    for(let i = 1; i <= num; i++) {
+        res=i*res;
+    }
+    return res;
+}
